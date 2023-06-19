a754e186a459a82f7b01e86541bd4b18b0e7e6d5
Joel Rebello <jrebello@packet.com>
2023-06-19 12:14:50 +0200

    rework bootstrap script to be handsfree

--- scripts/nats-bootstrap/functions.sh
+++ scripts/nats-bootstrap/functions.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
+function clean_natsserver() {
+	set +e
+	kubectl get statefulsets | grep nats && make clean-nats
+
+	set -e
+}
+
+function init_natsserver() {
+	make local-devel-upgrade
+}
+
+function clean_natsbox() {
+	kubectl exec -ti deployments/nats-box -- rm -rf /root/*.* /nsc
+}
+
+function init_natsaccounts() {
+
+	kubectl exec -ti deployments/nats-box -- /bin/sh <<'EOF'
+
 set -e
 
-export NKEYS_PATH=/root/nsc/nkeys
-export NSC_HOME=/root/nsc/accounts
-
-if [ ! -f .nsc.env ]; then
-	echo '
+echo '
 # NSC Environment Setup
 export NKEYS_PATH=/root/nsc/nkeys
 export NSC_HOME=/root/nsc/accounts
-' >nsc.env
-fi
+' > /root/.nsc.env
 
+source /root/.nsc.env
+
+mkdir -p $NSC_HOME
 mkdir -p "$NKEYS_PATH"
-mkdir -p "$NSC_HOME"
 
 # create operator account
 nsc add operator --name KO
@@ -77,6 +93,8 @@ nsc edit signing-key -a controllers --sk ${SK_A} \
 	--allow-pubsub '$JS.API.CONSUMER.CREATE.controllers.>' \
 	--allow-pubsub '$JS.API.CONSUMER.MSG.NEXT.controllers.>' \
 	--allow-pubsub '$JS.API.CONSUMER.DELETE.controllers.>' \
+	--allow-pubsub '$JS.API.STREAM.INFO.KV_active-controllers' \
+    --allow-pubsub '$JS.API.STREAM.CREATE.KV_active-controllers' \
 	--allow-pubsub '$JS.ACK.controllers.>' \
 	--allow-sub 'com.hollow.sh.serverservice.events.>' \
 	--allow-pubsub 'com.hollow.sh.controllers.>' \
@@ -93,40 +111,82 @@ nsc add user -a controllers --name alloy -K controllers
 # create flasher user, with the controllers role
 nsc add user -a controllers --name flasher -K controllers
 
-nsc generate config --sys-account SYS --nats-resolver
-echo ">>> accounts generated, now follow steps below.."
-echo "1. update values.yaml with Operator, SYS, Resolver Preload values from the above output."
-echo "2. update the nats-server helm install - make clean-nats && make local-devel-upgrade."
-echo "3. make sure the nats-server pod is restarted - and indicates its running, give it about 15-20s or so after its started"
-echo "4. hit enter when ready - this will push the accounts to the nats server."
-read
 
-source nsc.env
+EOF
+}
 
-echo ">>> pushing accounts to nats-server..."
-nsc push --system-account SYS -u nats://nats:4222 -A
+function kuexec() {
+	kubectl exec -ti deployments/nats-box -- /bin/sh -c \
+		"NKEYS_PATH=/root/nsc/nkeys NSC_HOME=/root/nsc/accounts && $1"
+}
 
-cat /root/nsc/nkeys/creds/KO/serverservice/serverservice.creds
-echo ">> add the above creds into templates/serverservice-nats-creds-configmap.yaml, press enter when done..."
-read
+function push_natsaccounts() {
+	while ! kubectl logs statefulsets/nats nats | head -100 | grep "Server is ready"; do
+		echo "waiting for nats server to be ready to push accounts... "
+		sleep 5
+	done
 
-cat /root/nsc/nkeys/creds/KO/controllers/alloy.creds
-echo ">> add the above creds into templates/alloy-nats-creds-configmap.yaml, press enter when done..."
-read
+	sleep 10
+	kuexec "nsc push --system-account SYS -u nats://nats:4222 -A"
+}
 
-cat /root/nsc/nkeys/creds/KO/controllers/conditionorc.creds
-echo ">>> add the above creds into templates/conditionorc-nats-creds-configmap.yaml, press enter when done..."
-read
+function update_valuesyaml() {
+	set -x
+	f=values.yaml.bk
+	cp values.yaml $f
 
-cat /root/nsc/nkeys/creds/KO/controllers/flasher.creds
-echo ">>> add the above creds into templates/flasher-nats-creds-configmap.yaml, press enter when done..."
-read
+	CURRENT_OPKEY=$(awk '/operator: /{print $2}' $f)
+	CURRENT_SYSKEY=$(awk '/systemAccount: /{print $2}' $f)
+	CURRENT_SYSPRELOADKEY=$(grep resolverPreload -a1 $f | grep $CURRENT_SYSKEY | awk -F': ' '/: /{print $2}')
 
-echo ">>> creating tarball with nsc accounts to restore these accounts"
-cd /
-tar -cvzf nats-accounts.tar.gz /root/nsc /nsc
-echo ">>> copy over /nats-accounts.tar.gz locally"
-echo "use kubectl cp $(hostname):/nats-accounts.tar.gz nats-accounts.tar.gz"
-echo "hit enter when complete..."
-read
-echo "all done."
+	OPKEY=$(kuexec "nsc generate config --sys-account SYS --nats-resolver | awk  '/operator: /{print \$2}' | tr -d '\\r' ")
+	SYSKEY=$(kuexec "nsc generate config --sys-account SYS --nats-resolver | awk '/system_account: /{print \$2}' | tr -d '\\r' ")
+	SYSPRELOADKEY=$(kuexec "nsc generate config --sys-account SYS  --nats-resolver |  \
+    grep resolver_preload -A1 | sed -e 's/,//g' -e 's/{//g'  | awk -F': ' '/: /{print \$2}' | tr -d '\r' ")
+
+	OPKEY=$(echo $OPKEY | tr -d '\r')
+	SYSKEY=$(echo $SYSKEY | tr -d '\r')
+	SYSPRELOADKEY=$(echo $SYSPRELOADKEY | tr -d '\n' | tr -d '\r' | tr -d ' ')
+	sed -ie 's/operator: '${CURRENT_OPKEY}'/operator: '${OPKEY}'/' $f
+	sed -ie 's/systemAccount: '${CURRENT_SYSKEY}'/systemAccount: '${SYSKEY}'/' $f
+	sed -ie 's/'${CURRENT_SYSKEY}': '${CURRENT_SYSPRELOADKEY}'/'${SYSKEY}': '${SYSPRELOADKEY}'/' $f
+
+	mv $f values.yaml
+}
+
+function push_controller_secrets() {
+	for controller in conditionorc alloy flasher; do
+		sekrit=$(kuexec "cat /root/nsc/nkeys/creds/KO/controllers/${controller}.creds" | base64)
+
+		echo "apiVersion: v1
+metadata:
+  name: ${controller}-secrets
+  namespace: default
+data:
+  ${controller}-nats-creds: $sekrit
+kind: Secret
+type: Opaque" >/tmp/kind_${controller}_secret.yaml
+
+		kubectl apply -f /tmp/kind_${controller}_secret.yaml
+	done
+}
+
+function push_serverservice_secrets() {
+	sekrit=$(kuexec "cat /root/nsc/nkeys/creds/KO/serverservice/serverservice.creds" | base64)
+
+	echo "apiVersion: v1
+metadata:
+  name: serverservice-secrets
+  namespace: default
+data:
+  serverservice-nats-creds: $sekrit
+kind: Secret
+type: Opaque" >/tmp/kind_serverservice_secret.yaml
+
+	kubectl apply -f /tmp/kind_serverservice_secret.yaml
+}
+
+function backup_accounts() {
+	kuexec "tar -czf nats-accounts.tar.gz /root/nsc /nsc"
+	kubectl cp $(kubectl get pods | awk '/nats-box/{print $1}'):/nats-accounts.tar.gz ./scripts/nats-bootstrap/nats-accounts.tar.gz
+}
