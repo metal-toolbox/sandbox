ad5bae5c703cc5ae52c067ca063349e1785e8154
Joel Rebello <jrebello@packet.com>
2023-07-31 15:25:19 +0200

    scripts/serverservice: update helper script to use flags

--- scripts/serverservice/add-server.go
+++ scripts/serverservice/add-server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
 
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-retryablehttp"
@@ -9,9 +12,32 @@ import (
 )
 
 func main() {
+
+	endpoint := flag.String("endpoint", "http://localhost:8000", "Serverservice endpoint")
+	serverID := flag.String("server-id", "", "Server UUID to be added into serverservice")
+	facilityCode := flag.String("facility", "", "Facility code (e.g fra9)")
+	bmcUser := flag.String("bmc-user", "", "BMC username")
+	bmcPass := flag.String("bmc-pass", "", "BMC password")
+	bmcAddr := flag.String("bmc-addr", "", "BMC address")
+
+	flag.Parse()
+
+	switch {
+	case *serverID == "":
+		log.Fatal("expected -server-id flag")
+	case *facilityCode == "":
+		log.Fatal("expected -facility flag")
+	case *bmcUser == "":
+		log.Fatal("expected -bmc-user flag")
+	case *bmcPass == "":
+		log.Fatal("expected -bmc-pass flag")
+	case *bmcAddr == "":
+		log.Fatal("expected -bmc-addr flag")
+	}
+
 	client, err := serverservice.NewClientWithToken(
 		"dummy",
-		"http://localhost:8000",
+		*endpoint,
 		retryablehttp.NewClient().StandardClient(),
 	)
 
@@ -21,10 +47,10 @@ func main() {
 
 	ctx := context.Background()
 
-	serverUUID := uuid.MustParse("")
+	serverUUID := uuid.MustParse(*serverID)
 
 	// Add server
-	server := serverservice.Server{UUID: serverUUID, Name: "ede81024", FacilityCode: "fra9"}
+	server := serverservice.Server{UUID: serverUUID, Name: serverUUID.String(), FacilityCode: *facilityCode}
 
 	_, _, err = client.Create(ctx, server)
 	if err != nil {
@@ -32,14 +58,14 @@ func main() {
 	}
 
 	// Add server BMC credential
-	_, err = client.SetCredential(ctx, serverUUID, "bmc", "root", "calvin")
+	_, err = client.SetCredential(ctx, serverUUID, "bmc", *bmcUser, *bmcPass)
 	if err != nil {
 		panic(err)
 	}
 
 	// Add server BMC IP attribute
-	bmcIPAttr := serverservice.Attributes{Namespace: "sh.hollow.bmc_info", Data: []byte(`{"address": "192.168.1.1"}`)}
-
+	addrAttr := fmt.Sprintf(`{"address": "%s"}`, *bmcAddr)
+	bmcIPAttr := serverservice.Attributes{Namespace: "sh.hollow.bmc_info", Data: []byte(addrAttr)}
 	_, err = client.CreateAttributes(ctx, serverUUID, bmcIPAttr)
 	if err != nil {
 		panic(err)
